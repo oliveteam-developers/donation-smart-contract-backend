546a99e8a66d3207795501ced42ff4ced945884e
DuongKien <ddk0705@gmail.com>
2022-05-09 17:31:37 +0700

    Refactor clean build folder

--- src/ethereum/clean.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const fs = require('fs-extra');
-
-const buildPath = path.resolve(__dirname, 'build');
-if (buildPath) {
-    fs.removeSync(buildPath);
-}
--- src/ethereum/compile.js
+++ src/ethereum/compile.js
@@ -2,6 +2,11 @@ const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 
+const buildPath = path.resolve(__dirname, 'build');
+if (buildPath) {
+    fs.removeSync(buildPath);
+}
+
 const contractPath = path.resolve(__dirname, 'contracts', 'Donation.sol');
 const source = fs.readFileSync(contractPath, 'UTF-8');
 
@@ -23,7 +28,6 @@ const input = {
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-const buildPath = path.resolve(__dirname, 'build');
 fs.ensureDirSync(buildPath);
 
 for (let contractName in output.contracts['Donation.sol']) {
