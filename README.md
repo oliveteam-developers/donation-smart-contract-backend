0311937143f2f60116e92264db6d244b53049aae
DuongKien <ddk0705@gmail.com>
2022-05-09 10:55:46 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Donation Smart Contract Backend
---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(cors());
+
+const router = require(__dirname + '/src/router');
+app.use(router);
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+app.listen(port, host, () => {
+  console.log('Server is running on port', port);
+});
